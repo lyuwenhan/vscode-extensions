c1b0a8ced04119b8077714f60de6c463bc96ec53
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2025-10-25 18:39:04 -0400

    Make createContributes.js more simpler

--- code-formatter-and-minifier/createContributes.js
+++ code-formatter-and-minifier/createContributes.js
@@ -11,6 +11,7 @@ const rid = {
 	jsonl: "resourceLangId == jsonl",
 	no: "false"
 };
+const a = {"true":"!editorHasSelection", "false":"editorHasSelection"}
 const contr = [{
 	command: "generateUuid",
 	title: "Generate UUID",
@@ -24,7 +25,7 @@ const contr = [{
 	title: "Minify current selection",
 	group: "92",
 	menus: {
-		"editor/context": " && editorHasSelection && (resourceLangId == javascript || resourceLangId == json || resourceLangId == jsonc || resourceLangId == jsonl)",
+		"editor/context": [true, ["js", "json", "jsonl"]],
 		commandPalette: ["js", "json", "jsonl"]
 	}
 }, {
@@ -32,7 +33,7 @@ const contr = [{
 	title: "Beautify current selection",
 	group: "93",
 	menus: {
-		"editor/context": " && editorHasSelection && (resourceLangId == javascript || resourceLangId == json || resourceLangId == jsonc || resourceLangId == jsonl)",
+		"editor/context": [true, ["js", "json", "jsonl"]],
 		commandPalette: ["js", "json", "jsonl"]
 	}
 }, {
@@ -40,7 +41,7 @@ const contr = [{
 	title: "Mitify current selection",
 	group: "94",
 	menus: {
-		"editor/context": " && editorHasSelection && resourceLangId == javascript",
+		"editor/context": [true, ["js"]],
 		commandPalette: ["js"]
 	}
 }, {
@@ -48,7 +49,7 @@ const contr = [{
 	title: "Sort current selection",
 	group: "95",
 	menus: {
-		"editor/context": " && editorHasSelection && (resourceLangId == json || resourceLangId == jsonc)",
+		"editor/context": [true, ["json"]],
 		commandPalette: ["json"]
 	}
 }, {
@@ -56,7 +57,7 @@ const contr = [{
 	title: "Minify current file",
 	group: "96",
 	menus: {
-		"editor/context": " && !editorHasSelection && (resourceLangId == javascript || resourceLangId == json || resourceLangId == jsonc || resourceLangId == jsonl)",
+		"editor/context": [false, ["js", "json", "jsonl"]],
 		commandPalette: ["js", "json", "jsonl"],
 		"explorer/context": ["js", "json", "jsonl"],
 		"editor/title/context": ["js", "json", "jsonl"]
@@ -66,7 +67,7 @@ const contr = [{
 	title: "Beautify current file",
 	group: "97",
 	menus: {
-		"editor/context": " && !editorHasSelection && (resourceLangId == javascript || resourceLangId == json || resourceLangId == jsonc || resourceLangId == jsonl)",
+		"editor/context": [false, ["js", "json", "jsonl"]],
 		commandPalette: ["js", "json", "jsonl"],
 		"explorer/context": ["js", "json", "jsonl"],
 		"editor/title/context": ["js", "json", "jsonl"]
@@ -76,7 +77,7 @@ const contr = [{
 	title: "Mitify current file",
 	group: "98",
 	menus: {
-		"editor/context": " && !editorHasSelection && resourceLangId == javascript",
+		"editor/context": [false, ["js"]],
 		commandPalette: ["js"],
 		"explorer/context": ["js"],
 		"editor/title/context": ["js"]
@@ -86,7 +87,7 @@ const contr = [{
 	title: "Sort current file",
 	group: "99",
 	menus: {
-		"editor/context": " && !editorHasSelection && (resourceLangId == json || resourceLangId == jsonc)",
+		"editor/context": [false, ["json"]],
 		commandPalette: ["json"],
 		"explorer/context": ["json"],
 		"editor/title/context": ["json"]
@@ -111,23 +112,23 @@ contr.forEach(e => {
 		ret.menus["editor/context"].push({
 			command: cmd + e.command,
 			group: groupName + e.group,
-			when: "editorTextFocus && (resourceScheme == 'file' || resourceScheme == 'untitled')" + e.menus["editor/context"]
+			when: "editorTextFocus && (resourceScheme == 'file' || resourceScheme == 'untitled')" + (e.menus["editor/context"] ? " && " + (e.menus["editor/context"][0] ? "" : "!") + "editorHasSelection && (" + e.menus["editor/context"][1].map(e => rid[e]).join(" || ") + ")" : "")
 		})
 	}
-	if (e.menus.commandPalette || e.menus.commandPalette === "") {
+	if (e.menus.commandPalette) {
 		ret.menus.commandPalette.push({
 			command: cmd + e.command,
 			when: e.menus.commandPalette.map(e => rid[e]).join(" || ")
 		})
 	}
-	if (e.menus["explorer/context"] || e.menus["explorer/context"] === "") {
+	if (e.menus["explorer/context"]) {
 		ret.menus["explorer/context"].push({
 			command: cmd + e.command,
 			group: groupName + e.group,
 			when: e.menus["explorer/context"].map(e => rid[e]).join(" || ")
 		})
 	}
-	if (e.menus["editor/title/context"] || e.menus["editor/title/context"] === "") {
+	if (e.menus["editor/title/context"]) {
 		ret.menus["editor/title/context"].push({
 			command: cmd + e.command,
 			group: groupName + e.group,
