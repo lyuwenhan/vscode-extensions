a5897f3e19d84034e23304629020961fd474d6c4
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2025-11-06 17:40:21 -0500

    Added build.js

---
+++ coin-flipper/build.js
@@ -0,0 +1,10 @@
+const esbuild = require("esbuild");
+esbuild.build({
+	entryPoints: ["./src/extension.js"],
+	bundle: true,
+	outfile: "out/extension.js",
+	external: ["vscode"],
+	format: "cjs",
+	platform: "node",
+	minify: true
+});
