e11263f00d44db3dd109e1506b96743098d635f9
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2025-11-09 17:10:16 -0500

    Added codemirror to replace textarea

--- sidebar-notes/docs/main.js
+++ sidebar-notes/docs/main.js
@@ -1,24 +1,143 @@
+const textarea = document.getElementById("notepad");
+const languageModes = {
+	"plain text": {
+		mode: "null"
+	},
+	c: {
+		mode: "text/x-csrc",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	cpp: {
+		mode: "text/x-c++src",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	css: {
+		mode: "css",
+		autoCloseBrackets: true,
+		autoCloseTags: true
+	},
+	java: {
+		mode: "text/x-java",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	json: {
+		mode: "javascript",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	html: {
+		mode: "htmlmixed",
+		matchBrackets: true,
+		autoCloseTags: true
+	},
+	shell: {
+		mode: "shell",
+		matchBrackets: true
+	},
+	python: {
+		mode: "python",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	markdown: {
+		mode: "markdown"
+	},
+	javascript: {
+		mode: "javascript",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	},
+	yaml: {
+		mode: "yaml"
+	},
+	sql: {
+		mode: "text/x-sql",
+		matchBrackets: true,
+		autoCloseBrackets: true
+	}
+};
+
+function tomode (mode) {
+	return {
+		theme: "default",
+		lineNumbers: true,
+		tabSize: 4,
+		indentUnit: 4,
+		indentWithTabs: true,
+		styleActiveLine: true,
+		...mode
+	}
+}
+let sendMessage = () => {};
+const codeEle = textarea;
+const codeLangEle = document.getElementById("lang-choose");
+let lang = codeLangEle.value || "plain text";
+var edtlang = languageModes[lang];
+let editor;
+
+function ed_init () {
+	editor?.toTextArea();
+	editor = CodeMirror.fromTextArea(codeEle, tomode(edtlang));
+	editor.getWrapperElement().classList.add("code-cm");
+	editor.setSize("auto", `${innerHeight}px`);
+	editor.on("change", () => {
+		editor.setSize("auto", `${innerHeight}px`);
+		sendMessage()
+	})
+}
+ed_init();
+codeLangEle.addEventListener("change", function () {
+	lang = this.value || "plain text";
+	edtlang = languageModes[lang];
+	ed_init();
+	sendMessage()
+});
 let fontSize = 16;
-const vscode = acquireVsCodeApi(),
-	textarea = document.getElementById("notepad");
 addEle = document.getElementById("add");
 equalEle = document.getElementById("equal");
 minusEle = document.getElementById("minus");
-vscode.postMessage({
-	type: "get"
-});
-
-function sendMessage () {
+if (window.acquireVsCodeApi) {
+	const vscode = acquireVsCodeApi();
 	vscode.postMessage({
-		type: "edit",
-		content: textarea.value,
-		fontSize
+		type: "get"
+	});
+	sendMessage = function () {
+		console.log({
+			type: "edit",
+			content: editor.getValue(),
+			fontSize,
+			lang
+		});
+		vscode.postMessage({
+			type: "edit",
+			content: editor.getValue(),
+			fontSize,
+			lang
+		})
+	};
+	window.addEventListener("message", event => {
+		const message = event.data;
+		if (message.type == "setup") {
+			console.log(message);
+			lang = message.lang ?? "plain text";
+			codeLangEle.value = lang
+			edtlang = languageModes[lang];
+			fontSize = +(message.fontSize ?? "16");
+			ed_init();
+			editor.getWrapperElement().style.fontSize = fontSize + "px";
+			editor.refresh();
+			editor.setValue(message.content ?? "");
+		}
 	})
 }
 
 function sendResize () {
 	sendMessage();
-	textarea.style.fontSize = fontSize + "px"
+	editor.getWrapperElement().style.fontSize = fontSize + "px";
+	editor.refresh()
 }
 
 function sizeUp () {
@@ -59,17 +178,6 @@ window.addEventListener("keydown", e => {
 	if (e.altKey && e.key === "-") {
 		e.preventDefault();
 		sizeDown()
-	}
-});
-textarea.addEventListener("input", () => {
-	sendMessage()
-});
-window.addEventListener("message", event => {
-	const message = event.data;
-	if (message.type == "setup") {
-		document.getElementById("notepad").value = message.content ?? "";
-		fontSize = +(message.fontSize ?? "16");
-		textarea.style.fontSize = fontSize + "px"
 	}
 });
 addEle.addEventListener("click", sizeUp);
--- sidebar-notes/src/extension.js
+++ sidebar-notes/src/extension.js
@@ -4,6 +4,7 @@ class LeftPanelWebviewProvider {
 	constructor (context) {
 		this.context = context;
 		this.content = "";
+		this.lang = "plain text";
 		this.fontSize = 16
 	}
 	refresh () {}
@@ -17,22 +18,26 @@ class LeftPanelWebviewProvider {
 		this.activateMessageListener(webviewView)
 	}
 	getHtml (webviewView) {
-		const scriptUri = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "main.js")));
-		const styleUri = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "main.css")));
-		return `<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="${styleUri}"></head><body><textarea id="notepad" placeholder="Write your notes here..."></textarea><div id="panel"><svg id="add" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 2L2 2L2 1L3 1L3 2L4 2L4 3L3 3L3 4L2 4L2 3L1 3Z"/></svg><svg id="equal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 1L4 1L4 2L1 2ZM1 3L4 3L4 4L1 4Z"/></svg><svg id="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 2L4 2L4 3L1 3Z"/></svg></div><script src="${scriptUri}"><\/script></body></html>`
+		const maincss = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "main.css")));
+		const codemirrorcss = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "codemirror.css")));
+		const codemirrorjs = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "codemirror.js")));
+		const mainjs = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(this.context.extensionPath, "docs", "main.js")));
+		return `<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="${maincss}"><link rel="stylesheet" href="${codemirrorcss}"><script src="${codemirrorjs}"><\/script></head><body><div class="choose-lang"><lable id="lang-text">launguage:</lable><select id="lang-choose" translate="no"><option value="plain text" selected="selected">plain&nbsp;text</option><option value="c">c</option><option value="cpp">c++</option><option value="css">css</option><option value="sql">sql</option><option value="java">java</option><option value="json">json</option><option value="html">html</option><option value="yaml">yaml</option><option value="shell">shell</option><option value="python">python</option><option value="markdown">markdown</option><option value="javascript">javascript</option></select></div><div class="well"><textarea id="notepad" placeholder="Write your notes here..."></textarea></div><div id="panel"><svg id="add" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 2L2 2L2 1L3 1L3 2L4 2L4 3L3 3L3 4L2 4L2 3L1 3Z"/></svg><svg id="equal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 1L4 1L4 2L1 2ZM1 3L4 3L4 4L1 4Z"/></svg><svg id="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 2L4 2L4 3L1 3Z"/></svg></div><script src="${mainjs}"><\/script></body></html>`
 	}
 	sendSetup (webviewView) {
 		webviewView.webview.postMessage({
 			type: "setup",
 			content: this.content,
-			fontSize: this.fontSize
+			fontSize: this.fontSize,
+			lang: this.lang
 		})
 	}
 	activateMessageListener (webviewView) {
 		webviewView.webview.onDidReceiveMessage(async message => {
 			if (message.type == "edit") {
 				this.content = message.content ?? "";
-				this.fontSize = +(message.fontSize ?? "16")
+				this.fontSize = +(message.fontSize ?? "16");
+				this.lang = message.lang ?? "plain text"
 			} else if (message.type == "get") {
 				this.sendSetup(webviewView)
 			}
