ec0fde67895a86061bbec35287cbb236ac5f9450
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2025-12-29 11:31:43 -0500

    Updated build.js

--- coin-flipper/build.js
+++ coin-flipper/build.js
@@ -1,19 +1,19 @@
 const esbuild = require("esbuild");
 const fs = require("fs");
-if (process.argv.slice(2).includes("--noMin")) {
-	fs.promises.mkdir("out", {
-		recursive: true
-	}).then(() => {
-		fs.promises.copyFile("./src/extension.js", "out/extension.js")
-	})
-} else {
-	esbuild.build({
-		entryPoints: ["./src/extension.js"],
-		bundle: true,
-		outfile: "out/extension.js",
-		external: ["vscode"],
-		format: "cjs",
-		platform: "node",
-		minify: true
-	})
-}
+fs.promises.mkdir("out", {
+	recursive: true
+}).then(() => {
+	if (process.argv.slice(2).includes("--noMin")) {
+		fs.writeFileSync("out/extension.js", 'module.exports=require("../src/extension.js")', "utf8")
+	} else {
+		esbuild.build({
+			entryPoints: ["./src/extension.js"],
+			bundle: true,
+			outfile: "out/extension.js",
+			external: ["vscode"],
+			format: "cjs",
+			platform: "node",
+			minify: true
+		})
+	}
+});
